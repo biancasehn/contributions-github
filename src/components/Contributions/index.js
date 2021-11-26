53d7c52ebccc2c918290fa808bb9d27305401800
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2021-11-26 16:57:58 +0100

    layout in css

--- src/components/Contributions/index.js
+++ src/components/Contributions/index.js
@@ -6,10 +6,10 @@ function Contributions() {
   let daysOneYear = [
     {
       day: dayjs().subtract(1, "year").format("DD"),
-      month: dayjs().subtract(1, "year").format("MM"),
+      month: dayjs().subtract(1, "year").format("MMM"),
       year: dayjs().subtract(1, "year").format("YYYY"),
       weekDay: dayjs().subtract(1, "year").format("dd"),
-
+      contributions: Math.random() * 20
     },
   ];
 
@@ -18,9 +18,10 @@ function Contributions() {
       ...daysOneYear,
       {
         day: day.format("DD"),
-        month: day.format("MM"),
+        month: day.format("MMM"),
         year: day.format("YYYY"),
         weekDay: day.format("dd"),
+        contributions: Math.random() * 20
       },
     ];
 
@@ -31,46 +32,26 @@ function Contributions() {
     ...daysOneYear,
     {
       day: dayjs().format("DD"),
-      month: dayjs().format("MM"),
+      month: dayjs().format("MMM"),
       year: dayjs().format("YYYY"),
       weekDay: dayjs().format("dd"),
+      contributions: Math.random() * 20
     },
   ];
 
   return (
     <div className={style.frame}>
-      <div className={style.months}>
-        <p>{dayjs().format("MMM")}</p>
-        <p>{dayjs().add(1, "month").format("MMM")}</p>
-        <p>{dayjs().add(2, "month").format("MMM")}</p>
-        <p>{dayjs().add(3, "month").format("MMM")}</p>
-        <p>{dayjs().add(4, "month").format("MMM")}</p>
-        <p>{dayjs().add(5, "month").format("MMM")}</p>
-        <p>{dayjs().add(6, "month").format("MMM")}</p>
-        <p>{dayjs().add(7, "month").format("MMM")}</p>
-        <p>{dayjs().add(8, "month").format("MMM")}</p>
-        <p>{dayjs().add(9, "month").format("MMM")}</p>
-        <p>{dayjs().add(10, "month").format("MMM")}</p>
-        <p>{dayjs().add(11, "month").format("MMM")}</p>
-        <p>{dayjs().add(12, "month").format("MMM")}</p>
-
-      </div>
-      <div className={style.weeks}>
-        <p>Sun</p>
-        <p>Mon</p>
-        <p>Tue</p>
-        <p>Wed</p>
-        <p>Thu</p>
-        <p>Fri</p>
-        <p>Sat</p>
-      </div>
       <div className={style.board}>
         {daysOneYear.map((dia, index) => {
           return (
               <div
                 key={index}
                 className={index === 0 ? style[`weekDay${dia.weekDay}`] : null}
-              ><div className={style.rect}></div></div>
+                >
+                {index !== 0 && dia.month !== daysOneYear[index - 1].month ? <div className={style.months}>{dia.month}</div> : null}
+                {index < 7 && <div className={style.weeks}>{dia.weekDay}</div>}
+                <div className={style.rect}></div>
+              </div>
           );
         })}
       </div>
